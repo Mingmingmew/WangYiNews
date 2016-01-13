81ea3c82d7cd6fa79d9523aaa11f568b3c6daf93
何卫明 <e452860128@gmail.com>
2016-01-13 22:09:26 +0800

    新建项目

---
+++ NeteaseNews/NeteaseNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeteaseNews
+//
+//  Created by ming on 16/1/13.
+//  Copyright © 2016年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NeteaseNews/NeteaseNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NeteaseNews
+//
+//  Created by ming on 16/1/13.
+//  Copyright © 2016年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
